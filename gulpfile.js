b7d9c6e4cd2547db4b2490fbc2cabba8add48b94
Thomas Poulet <1039134+Lord-Nazdar@users.noreply.github.com>
2017-10-28 09:39:00 +0200

    Update frontend leagacy

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ var minifyCSS = require('gulp-csso');
 var cssimport = require("gulp-cssimport");
 
 gulp.task('css', function(){
-    return gulp.src('photo_scout_frontend/res/style/*.scss')
+    return gulp.src('photo_scout_frontend_legacy/res/style/*.scss')
         .pipe(cssimport())
         .pipe(sass())
         .pipe(minifyCSS())
-        .pipe(gulp.dest('photo_scout_frontend/assets'))
+        .pipe(gulp.dest('photo_scout_frontend_legacy/assets'))
 });
 
 gulp.task('default', [ 'css' ]);
 
 gulp.task('sass:watch', function () {
-    gulp.watch('photo_scout_frontend/res/style/*.scss', ['css']);
+    gulp.watch('photo_scout_frontend_legacy/res/style/*.scss', ['css']);
 });
